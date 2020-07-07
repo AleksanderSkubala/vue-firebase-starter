a28aa49d5bdaeb6683948278b4555b53ec650c37
AleksanderSkubala <alexskubala@gmail.com>
2020-07-07 21:36:18 +0200

    Add button

---
+++ src/components/SigninButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <button>Sign In with Google</button>
+</template>
+
+<script>
+export default {
+  name: 'SigninButton',
+};
+</script>
+
+<style lang="scss" scoped>
+  button {
+    border: 3px solid black;
+    background: none;
+    position: relative;
+    padding: 13px;
+    font-weight: bold;
+    font-size: 1em;
+
+    transition: color .3s ease-out;
+
+    &::after {
+      content: '';
+      top: 0;
+      left: 0;
+      z-index: -1;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color:darkorchid;
+
+      transform: scaleX(0);
+      transition: transform .3s ease-out;
+    }
+
+    &:hover {
+      color: white;
+    }
+
+    &:hover::after {
+      transform: scaleX(1);
+    }
+  }
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="login">
     <h1>Login</h1>
-    <button @click="login()">Login with google</button>
+    <SigninButton @click.native="login()"></SigninButton>
   </div>
 </template>
 
 <script>
 import { auth, googleProvider } from '@/initFirebase';
+import SigninButton from '@/components/SigninButton.vue';
 
 export default {
   name: 'Login',
+  components: {
+    SigninButton,
+  },
   data() {
     return {
       form: {},
